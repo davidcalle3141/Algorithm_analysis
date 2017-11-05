d09a45639e415e5966463e28d528da4b9f214438
davidcalle7229 <davidcalle3141@gmail.com>
2017-11-05 15:34:02 -0500

    finished writing

--- src/sorting_project/Main.java
+++ src/sorting_project/Main.java
@@ -1,16 +1,21 @@
 package sorting_project;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
         int [] array;
         Reader reader = new Reader("C:\\Users\\david calle\\IdeaProjects\\Algorithm_analysis\\textFiles\\unsortedTx");
         Sorter sorter = new Sorter();
+        Writer write = new Writer("C:\\Users\\david calle\\IdeaProjects\\Algorithm_analysis\\textFiles\\sortedTxt");
         reader.createArrayList();
 
        array = sorter.quickSort(reader.getArray(0));
 
+
+       write.createFileForArray(array, "quickSort");
        int t =5;
 
     }
--- src/sorting_project/Writer.java
+++ src/sorting_project/Writer.java
@@ -1,4 +1,43 @@
 package sorting_project;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Writer {
+    File outFile;
+    private String dir;
+    Writer(String directory){
+        this.dir = directory;
+
+
+    }
+    public void createFileForArray(int[]A, String sortType) throws IOException {
+        outFile = new File(dir +"/"+sortType+"Num"+A.length+".txt");
+        if(!outFile.createNewFile()){
+
+            System.out.println("file overwritten");}
+        writeToFile(A, outFile);
+    }
+
+    private void writeToFile(int[]A, File writeTo){
+        FileWriter fwriter = null;
+        BufferedWriter writer = null;
+
+        try{
+            fwriter = new FileWriter(writeTo, false);
+            writer = new BufferedWriter(fwriter);
+
+            for (int child: A) {
+                writer.write(Integer.toString(A[child-1]));
+                writer.newLine();
+
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
